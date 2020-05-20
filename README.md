b7f69206cc2c57d4916b4e814bf34afcf9cf1737
Kristof <kristof.kovacs@telenav.com>
2020-05-20 21:07:50 +0300

    new version

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # kjson
+![python_version](https://img.shields.io/static/v1?label=Python&message=3.5%20|3.6%20|3.7&color=blue) [![PyPI downloads/month](https://img.shields.io/pypi/dm/kjson?logo=pypi&logoColor=white)](https://pypi.python.org/pypi/kjson)
+
+## Description
+some json util methods/shorthands
+
+## Install
+~~~~bash
+pip install kjson
+# or
+pip3 install kjson
+~~~~
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools, os
+
+if os.path.exists(os.path.join(os.getcwd(), README.md)):
+    with open("README.md", "r") as f:
+        long_description = f.read()
+else:
+    long_description = 'kjson'
+
+setuptools.setup(
+    name="kjson",
+    version="0.0.1",
+    author="Kristof",
+    description="some json util methods/shorthands
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/kjson",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
